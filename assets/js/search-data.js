5ba796605a31d54636d8a3e5bd02d3c97d9f2107
joelleayoub <160648550+joelleayoub@users.noreply.github.com>
2026-04-30 15:05:49 +0000

    Deploying to gh-pages from @ joelleayoub/joelleayoub.github.io@3980eb546a1f98bf4dac9df834d5b479f2c34256 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
+  },{id: "nav-experience",
+          title: "experience",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/experience/";
+          },
+        },{id: "nav-projects",
           title: "projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
